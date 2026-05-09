12f9d7167c43fc3b132eb59430a3c75d1dda96f4
Ritika Saini <ritikasainii1234@gmail.com>
2026-05-10 01:48:30 +0530

    attendance_results

--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -9,6 +9,7 @@ from src.database.db import check_teacher_exists, create_teacher, teacher_login,
 from src.components.dialog_create_subject import create_subject_dialog
 from src.components.dialog_share_subject import share_subject_dialog
 from src.pipelines.face_pipeline import predict_attendance
+from src.components.dialog_add_photo import add_photos_dialog
 
 import numpy as np
 
